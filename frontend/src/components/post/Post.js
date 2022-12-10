0573ebdcd55f3f5e194815e9f1b07e494e10f425
Joseph Lander <josephclander@gmail.com>
2022-12-10 14:29:00 +0000

    Add styling for comments & background

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { Link } from 'react-router-dom';
 
 const Post = ({ post, callback }) => {
-  const user_id = window.localStorage.getItem("user_id");
-  const token = window.localStorage.getItem("token");
+  const user_id = window.localStorage.getItem('user_id');
+  const token = window.localStorage.getItem('token');
 
   const [details, setDetails] = useState(false);
   const isPostLikedByUser = post.likes.includes(user_id);
   const [isLiked, toggleIsLiked] = useState(isPostLikedByUser);
-  const [comments, setComment] = useState("");
+  const [comments, setComment] = useState('');
 
   const handleLikeToggle = async () => {
     toggleIsLiked((prevState) => !prevState);
 
     if (user_id) {
       let response = await fetch(`/posts/${post._id}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ type: "likes", isLiked, user_id }),
+        body: JSON.stringify({ type: 'likes', isLiked, user_id }),
       });
       const data = await response.json();
       if (response.status !== 202) {
@@ -32,22 +32,24 @@ const Post = ({ post, callback }) => {
     }
   };
 
-  const handleCommentSubmit = async (event) => { 
+  const handleCommentSubmit = async (event) => {
     event.preventDefault();
-    if (user_id && comments !== "") {
-
-      const {format} = require('date-fns')
+    if (user_id && comments !== '') {
+      const { format } = require('date-fns');
       let date = format(new Date(), 'dd.MM.yy');
       let time = format(new Date(), 'HH:mm');
 
       let response = await fetch(`/posts/${post._id}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ type: "comments", comments: comments + " at " + time + " on " + date })
-        });
+        body: JSON.stringify({
+          type: 'comments',
+          comments: comments + ' at ' + time + ' on ' + date,
+        }),
+      });
       const data = await response.json();
       if (response.status !== 202) {
         console.log(data.error);
@@ -55,21 +57,21 @@ const Post = ({ post, callback }) => {
         callback(true);
       }
     }
-    setComment("");
+    setComment('');
   };
 
   const handleCommentChange = (event) => {
-    setComment(event.target.value)
-  }
+    setComment(event.target.value);
+  };
 
   const messageFilter = (message) => {
-    const button = (          
-      <>         
+    const button = (
+      <>
         <button
-          className="btn-message"
+          className='btn-message'
           onClick={() => setDetails((prev) => !prev)}
         >
-          {details ? "Show less" : "Show more"}
+          {details ? 'Show less' : 'Show more'}
         </button>
       </>
     );
@@ -77,26 +79,37 @@ const Post = ({ post, callback }) => {
       let formatted = message.split(' ').slice(0, 30).join(' ');
       return (
         <>
-        {details ? message + ' ' : formatted + '... '}
-        {button}
+          {details ? message + ' ' : formatted + '... '}
+          {button}
         </>
       );
     } else {
       return message;
     }
-  }
+  };
+
+  const commentFormatter = (comment) => {
+    const timestamp = comment.slice(-20);
+    const formatted = comment.slice(0, -20);
+    return (
+      <p className='comment-output'>
+        {formatted}
+        <span className='comment-timestamp'>({timestamp})</span>
+      </p>
+    );
+  };
 
   return (
-    <div className="post">
-      <div className="post-header">
+    <div className='post'>
+      <div className='post-header'>
         <img
           className='user-icon'
           alt='user-icon'
-          src= {post.user_id.profile_pic}
+          src={post.user_id.profile_pic}
         />
 
-        <div className="post-header-info">
-          <Link className="link-profile" to={"/users/" + post.user_id._id}>
+        <div className='post-header-info'>
+          <Link className='link-profile' to={'/users/' + post.user_id._id}>
             {post.user_id.full_name}
           </Link>
           <label>
@@ -104,24 +117,24 @@ const Post = ({ post, callback }) => {
           </label>
         </div>
       </div>
-      <div className="post-body">
-        <article className="post-message" data-cy="post" key={post._id}>
+      <div className='post-body'>
+        <article className='post-message' data-cy='post' key={post._id}>
           {messageFilter(post.message)}
         </article>
       </div>
-      <div className="post-footer">
-        <button className="btn-details" onClick={handleLikeToggle}>
+      <div className='post-footer'>
+        <button className='btn-details' onClick={handleLikeToggle}>
           {isLiked ? (
             <img
-              className="img_likes"
-              src="../heart_full.svg"
-              alt="full heart"
+              className='img_likes'
+              src='../heart_full.svg'
+              alt='full heart'
             />
           ) : (
             <img
-              className="img_likes"
-              src="../heart_empty.svg"
-              alt="empty heart"
+              className='img_likes'
+              src='../heart_empty.svg'
+              alt='empty heart'
             />
           )}
         </button>
@@ -130,18 +143,27 @@ const Post = ({ post, callback }) => {
 
       <form className='post-body' onSubmit={handleCommentSubmit}>
         <input
-          placeholder="Post a comment!"
-          id="comments"
-          type="text"
+          className='comment-input'
+          placeholder='Post a comment!'
+          id='comments'
+          type='text'
           value={comments}
           onChange={handleCommentChange}
         />
-        <input id="submit" type="submit" value="Comment" />
+        <input
+          className='comment-submit'
+          id='submit'
+          type='submit'
+          value='Comment'
+        />
       </form>
-      
+
       <div className='post-body'>
         <article>
-          {post.comments.map((item) => <div>{item}</div>)}
+          {post.comments.map((item) =>
+            commentFormatter(item)
+            // <p className='comment-output'>{item}</p>
+          )}
         </article>
       </div>
     </div>
